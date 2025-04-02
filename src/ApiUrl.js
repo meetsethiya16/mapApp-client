719356412c9269c9b578cbcb341ad0053476a45a
meetsethiya16 <meet.sethiya.16@gmail.com>
2025-04-02 16:00:45 +0530

    updated backend url

--- src/ApiUrl.js
+++ src/ApiUrl.js
@@ -1,2 +1,4 @@
 export const apiUrlUser =
   "https://mapapp-server-production-0e9d.up.railway.app";
+export const apiUrlDashboard =
+  "https://mapapp-server-production-0e9d.up.railway.app/dashboard";
