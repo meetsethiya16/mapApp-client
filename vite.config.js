dda5ac43f59462ac7d768f538100f81d7dd4be96
meetsethiya16 <meet.sethiya.16@gmail.com>
2025-04-03 15:58:07 +0530

    unresolved cors error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+// import basicSsl from "@vitejs/plugin-basic-ssl";
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://localhost",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+//   plugins: [cssInjectedByJsPlugin(), react(), basicSsl()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         entryFileNames: "main-bundle.js",
+//         assetFileNames: "assets/[name][extname]",
+//       },
+//     },
+//   },
+// });
